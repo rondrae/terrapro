0aa544c749726f4b0c1f9178a5d6a3ec5ca43f20
Rondrae Reid <rondrae@gmail.com>
2021-08-12 15:27:13 -0400

    .

--- main.tf
+++ main.tf
@@ -92,8 +92,12 @@ output "aws_ami_id"{
     value = data.aws_ami.latest-amazon-linux-image.id
 }
 
+output "ec2_public_ip"{
+    value = aws_instance.myapp-server.public_ip
+}
+
 resource "aws_key_pair" "ssh-key" {
-    key_name = "server-key"
+    key_name = "terra"
     public_key = "${file(var.public_key_location)}"
   
 }
